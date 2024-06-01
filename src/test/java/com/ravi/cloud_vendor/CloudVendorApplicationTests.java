cff86a229117465b982646ffd07fd1052546848c
Ravi Prakash <raviprakash951998@gmail.com>
2024-06-01 12:34:30 +0530

    this is the first commit

---
+++ src/test/java/com/ravi/cloud_vendor/CloudVendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.cloud_vendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudVendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
